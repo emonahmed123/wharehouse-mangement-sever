84166e74e96e1220a79e8c7b8446f54a8b2281b1
emonahmed123 <imonshomon@gmail.com>
2022-04-29 18:46:23 -0700

    added getPost

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 const express = require('express');
 const cors = require('cors');
 require ('dotenv').config();
-const app =express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const app =express();
 const port =process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rkxze.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.njuiq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-async function run(){
-try{
- await client.connect()
- const bikecollection = client.db("bikewarehouse").collection("bike");
-}
-finally{
+console.log(uri)
+async function run() {
+    try{
+        await client.connect();
+        const bikeCollection =client.db('emonbikes').collection('bike');
+                      console.log('all cleare')
+      
+        app.get('/bike',async(req,res)=>{
+              const query={};
+              const cursor=bikeCollection.find(query)
+              const bikes =await cursor.toArray()
+res.send(bikes)
+        })
 
+    }
+  finally{
+
+  }
 }
-}
-run().catch(console.dir)
+run().catch(console.dir);
 
 app.get('/',(req,res)=>{
     res.send('Runing Server')
